8c514a6864251ae71f9693a94d79bc7c3024c256
esra nur özüm <esraozum@sabanciuniv.edu>
2022-06-30 14:05:34 +0300

    final version of the project

---
+++ server_application/server_application/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace server_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_listen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1_port = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1_listen
+            // 
+            this.button1_listen.Location = new System.Drawing.Point(330, 25);
+            this.button1_listen.Name = "button1_listen";
+            this.button1_listen.Size = new System.Drawing.Size(135, 59);
+            this.button1_listen.TabIndex = 0;
+            this.button1_listen.Text = "Listen";
+            this.button1_listen.UseVisualStyleBackColor = true;
+            this.button1_listen.Click += new System.EventHandler(this.button1_listen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // textBox1_port
+            // 
+            this.textBox1_port.Location = new System.Drawing.Point(94, 47);
+            this.textBox1_port.Name = "textBox1_port";
+            this.textBox1_port.Size = new System.Drawing.Size(209, 22);
+            this.textBox1_port.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 141);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(424, 229);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 415);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1_port);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1_listen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_listen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1_port;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ server_application/server_application/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace server_application
+{
+    public partial class Form1 : Form
+    {
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+
+        bool terminating = false;
+        bool listening = false;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+        private void button1_listen_Click(object sender, EventArgs e)
+        {
+            int serverPort;
+
+            if (Int32.TryParse(textBox1_port.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(3);
+
+                listening = true;
+                button1_listen.Enabled = false;
+                //textBox2_message.Enabled = true;
+                //button2_send.Enabled = true;
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                richTextBox1.AppendText("Started listening on port: " + serverPort + "\n");
+
+            }
+            else
+            {
+                richTextBox1.AppendText("Please check port number \n");
+            }
+        }
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+                    clientSockets.Add(newClient);
+                    richTextBox1.AppendText("A client is connected. \n");
+
+                    Thread receiveThread = new Thread(() => Receive(newClient));
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        richTextBox1.AppendText("The socket stopped working.\n");
+                    }
+                }
+            }
+        }
+        private void Receive(Socket thisClient)
+        {
+
+            bool connected = true;
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incominMessage = Encoding.Default.GetString(buffer);
+                    using (StreamWriter file = new StreamWriter(@"C:\Users\ESRA\OneDrive\Masaüstü\database.txt", append: true))
+                    {
+                        file.WriteLine(incominMessage);
+                    }
+
+                    incominMessage = incominMessage.Substring(0, incominMessage.IndexOf("\0"));
+                    richTextBox1.AppendText("Client with the username " + incominMessage.Substring(0, incominMessage.IndexOf("-")) + " has created an account\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+
+                        richTextBox1.AppendText("Client has disconnected\n");
+                    }
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                    connected = false;
+                }
+            }
+        }    
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+    }
+}
